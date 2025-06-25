edbd3ca73972cbc1ea30bd06cc094946fb5987a9
DoHuong25 <huong.dl225851@sis.hust.edu.vn>
2025-06-25 22:05:52 +0700

    up

--- backend/src/main/java/com/ISD/AIMS/DataSeeder.java
+++ backend/src/main/java/com/ISD/AIMS/DataSeeder.java
@@ -33,8 +33,8 @@ public class DataSeeder implements CommandLineRunner {
             book1.setQuantity(50);
             book1.setAuthors("Paulo Coelho");
             book1.setGenre("Phiêu lưu");
-            // URL ảnh đã được sửa
-            book1.setImageUrl("https://salt.tikicdn.com/cache/w1200/ts/product/d1/1a/35/9168953181585034a740702fba7c92d5.jpg");
+            // URL ảnh đang hoạt động
+            book1.setImageUrl("https://cdn0.fahasa.com/media/catalog/product/i/m/image_195509_1_36793.jpg");
             bookRepository.save(book1);
 
             Book book2 = new Book();
@@ -44,22 +44,22 @@ public class DataSeeder implements CommandLineRunner {
             book2.setQuantity(100);
             book2.setAuthors("Dale Carnegie");
             book2.setGenre("Self-help");
-            // URL ảnh đã được sửa
-            book2.setImageUrl("https://salt.tikicdn.com/cache/w1200/ts/product/55/63/80/c854734340d8299d940b5f0535384218.jpg");
+            // URL ảnh mới, đã kiểm tra
+            book2.setImageUrl("https://bizweb.dktcdn.net/100/418/683/products/dac-nhan-tam-biamem-2019-2-h500.jpg");
             bookRepository.save(book2);
 
 
             // --- Tạo CD ---
             CD cd1 = new CD();
-            cd1.setTitle("ABBA Gold");
+            cd1.setTitle("Vàng ABBA");
             cd1.setCategory("Âm nhạc");
             cd1.setPrice(250000);
             cd1.setQuantity(20);
             cd1.setArtist("ABBA");
             cd1.setGenre("Pop");
             cd1.setReleaseDate(LocalDate.of(1992, 9, 21));
-            // URL ảnh đã được sửa
-            cd1.setImageUrl("https://m.media-amazon.com/images/I/71tb1s3gV2L._UF1000,1000_QL80_.jpg");
+            // URL ảnh mới, đã kiểm tra
+            cd1.setImageUrl("https://upload.wikimedia.org/wikipedia/en/3/3c/Abba_gold.png");
             cdRepository.save(cd1);
 
 
